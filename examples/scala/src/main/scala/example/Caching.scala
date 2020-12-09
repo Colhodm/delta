30cea3df901c8419c4c61e44443dcd11e01100fc
Willis Wang <wwang4292@gmail.com>
2020-12-08 21:57:58 -0800

    Fix cache loading rows

--- examples/scala/src/main/scala/example/Caching.scala
+++ examples/scala/src/main/scala/example/Caching.scala
@@ -1,19 +1,3 @@
-/*
- * Copyright (2020) The Delta Lake Project Authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package example
 
 import org.apache.spark.{SparkConf, SparkContext}
@@ -24,54 +8,43 @@ import org.apache.spark.sql.functions._
 import org.apache.commons.io.FileUtils
 import java.io.File
 
-object QuickstartCaching {
+object Caching {
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession
       .builder()
-      .appName("Caching")
+      .appName("Quickstart")
       .master("local[*]")
       .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
       .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
       .getOrCreate()
 
+
     val file = new File("/tmp/delta-table")
     if (file.exists()) FileUtils.deleteDirectory(file)
-    
     // Create a table
     println("Creating a table")
     val path = file.getCanonicalPath
-    var data = spark.range(0, 5)
+    var data = spark.range(0, 100)
     data.write.format("delta").save(path)
 
     // Read table
-    println("Reading the table")
-    val df = spark.read.format("delta").load(path)
-    df.show()
-
-    // Upsert (merge) new data
-    println("Upsert new data")
-    val newData = spark.range(0, 20).toDF
     val deltaTable = DeltaTable.forPath(path)
+    println("Reading table")
+    for( a <- 1 to 10){ 
+      val r = new scala.util.Random
+      val r1 = r.nextInt(100)
+      val t0 = System.nanoTime()
 
-    deltaTable.as("oldData")
-      .merge(
-        newData.as("newData"),
-        "oldData.id = newData.id")
-      .whenMatched
-      .update(Map("id" -> col("newData.id")))
-      .whenNotMatched
-      .insert(Map("id" -> col("newData.id")))
-      .execute()
+        println(deltaTable.readRow((r1).toString),r1)
 
-    deltaTable.toDF.show()
+      val t1 = System.nanoTime()
 
-    for (x <- 1 to 20) {
-      println(deltaTable.readRow(x.toString))
+      println("Elapsed time: " + (t1 - t0) + "ns")
     }
-
     // Cleanup
     FileUtils.deleteDirectory(file)
+    
     spark.stop()
   }
 }
